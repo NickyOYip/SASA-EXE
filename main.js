84f7d6e957c20c2b7442b120de1bcd77629ef735
NickyOYip <150511100+NickyOYip@users.noreply.github.com>
2025-07-02 11:48:41 +0800

    Refactor index.html and main.js to remove placeholder content; update style.css for base styles

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
 import './style.css'
 
-// Simple counter functionality
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button">Count: 0</button>
-    </div>
-    <p class="read-the-docs">
-      Click on the button to see the counter in action!
-    </p>
-  </div>
-`
-
-let counter = 0
-
-document.querySelector('#counter').addEventListener('click', () => {
-  counter++
-  document.querySelector('#counter').textContent = `Count: ${counter}`
-})
+// Your JavaScript code goes here
