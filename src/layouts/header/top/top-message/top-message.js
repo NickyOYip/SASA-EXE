a9d38bd31f290723603461ff04ac1e717d88eefb
NickyOYip <150511100+NickyOYip@users.noreply.github.com>
2025-07-07 14:52:27 +0800

    fix: update icon styles in top message component for better alignment

--- src/layouts/header/top/top-message/top-message.js
+++ src/layouts/header/top/top-message/top-message.js
@@ -16,7 +16,7 @@ export class TopMessage {
       <a data-qe-id="top_message" href="//www.sasa.com.hk/page/PayMe" class="top-message__link">
         <span class="top-message__content">
           Grab up to $100 in PayMe App flash vouchers! 💰
-          <i class="ico ico-chevron-right top-message__arrow"></i>
+          <i class="ico ico-chevron-right" style="text-decoration: none;vertical-align: middle;margin-left: 8px;font-size: 16px;font-weight: 600;"></i>
         </span>
       </a>
     `;
